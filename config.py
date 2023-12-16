2cf67eef74062c93543431cc41450b2693cbfec1
AF2024 <81873226+AF2024@users.noreply.github.com>
2023-12-16 20:48:42 +0330

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Development(Config):
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "mongodb+srv://AF2024:amirrezaL90@cluster0.ospggan.mongodb.net/?retryWrites=true&w=majority"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "Your value"
+    STRING_SESSION = "1BJWap1wBu2MEKc2PVtsg_5aOleCsr05lB62q0TQgSd_ELXQ82Ns-0ZVuw9GvqyRx-alwjYG3U3-hBsLhIYOGZBI2Y28kQtT4HEXF4vTUPiDa8poJzmdSKQkV_AHlpGZe0HpjL69Rsc9gLFQcvjk-TI5qipDfGev5u5ot77Fm1KQLspKYZvJgCLFMr-gwM3tUGLoLHfEACjWVJrOTSpeFConrsBaYnHGDASkMcWPoaPVzveBXawBq0_DRmDYQjuZfOPzyA-XAL8TirjQC9uYvt715EMBxaYaEKUUYKjJNmdODVerwxCjTkf6W5FoUux1zFy8I_nkRYGZ2jkscpIar-OFzYL3OPho="
     # create a new bot in @botfather and fill the following vales with bottoken
     TG_BOT_TOKEN = "5108409464:AAGPhvkZL6ReRtvMNUrnSJ6ilg53aauGvmU"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
